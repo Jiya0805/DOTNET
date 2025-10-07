2a3b2018ee15bdfe56c3bc6ca544234be3441446
Jiya Shah <jiyashah2406@gmail.com>
2025-10-07 13:00:11 +0530

    Add files via upload

---
+++ relayform/relayform/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace relayform.commands
+{
+    public class MainViewModel
+    {
+        public RelayCommand showMessageCommand { get; }
+        public MainViewModel()
+        {
+            showMessageCommand = new RelayCommand(_ => showMessage());
+        }
+            public void showMessage()
+            {
+                System.Windows.MessageBox.Show("Hello RelayCommand Works !");
+            }
+        
+    }
+}
