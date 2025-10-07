4cbc5288afc91cfa2cdc42ddbec7928b72699a1f
Jiya Shah <jiyashah2406@gmail.com>
2025-10-07 13:29:29 +0530

    Add files via upload

---
+++ relayform_predicate/relayform_predicate/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using relayform_predicate.Relay;
+using relayform_predicate;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace relayform_predicate.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public RelayCommand GreetCommand { get; set; }
+
+        private string _username;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; OnPropertyChanged(nameof(Username)); }
+        }
+
+        public MainViewModel()
+        {
+            GreetCommand = new RelayCommand(_ => { Greet(); });
+        }
+
+        public void Greet()
+        {
+            MessageBox.Show("Username: " + Username);
+        }
+    }
+}
